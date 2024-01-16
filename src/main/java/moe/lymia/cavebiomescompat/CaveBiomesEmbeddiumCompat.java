1457a8d69db373f651a4206402c4bc60da6410ce
Alissa Rao <lymia@lymia.moe>
2024-01-16 09:23:28 -0800

    We're just going to coremod rather than forking.

---
+++ src/main/java/moe/lymia/cavebiomescompat/CaveBiomesEmbeddiumCompat.java
@@ -0,0 +1,8 @@
+package moe.lymia.cavebiomescompat;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(value = CaveBiomesEmbeddiumCompat.MOD_ID)
+public class CaveBiomesEmbeddiumCompat {
+    public static final String MOD_ID = "cavebiomeapi-embeddiumcompat";
+}
